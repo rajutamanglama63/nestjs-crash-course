6f0a9a5a496b9ebcdb12552d203de305bf86e394
rajutamanglama63 <rajutamanglama63@gmail.com>
2023-04-10 14:16:06 +0545

    feat: handling auth from middleware

--- src/users/middlewares/example-middleware/example-middleware.middleware.ts
+++ src/users/middlewares/example-middleware/example-middleware.middleware.ts
@@ -1,9 +1,19 @@
-import { Injectable, NestMiddleware } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable, NestMiddleware } from '@nestjs/common';
 
 @Injectable()
 export class ExampleMiddlewareMiddleware implements NestMiddleware {
-  use(req: any, res: any, next: () => void) {
+  use(req: Request, res: Response, next: () => void) {
     console.log("Proof of middleware running.")
-    next();
+
+    const authorization = req.headers["authorization"]
+
+    if(!authorization) {
+      throw new HttpException("Invalid token!!", HttpStatus.FORBIDDEN)
+    }
+
+    if(authorization === "12345asdf") {
+
+      next();
+    }
   }
 }
